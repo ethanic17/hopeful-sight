cd000e57440939dbb9578b5bd67477183165b14b
Koby Kern <kobykern01@gmail.com>
2024-10-15 14:23:07 -0700

    I am so sorry... again...

--- application/Backend/hopefulsight/hopefulsight/settings.py
+++ application/Backend/hopefulsight/hopefulsight/settings.py
@@ -17,6 +17,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# In settings.py
+FORCE_SCRIPT_NAME = None
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
