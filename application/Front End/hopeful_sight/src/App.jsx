51988edd3bc849e0cf1e7898e69309a8187afd2b
Miguel <miguelmaurer456@gmail.com>
2024-10-28 15:02:22 -0700

    fixed the routes and set up the home route to display a gallery view of glasses

--- application/Front End/hopeful_sight/src/App.jsx
+++ application/Front End/hopeful_sight/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Account } from "./pages/Account";
 import { Map } from "./pages/Map";
-import { glassesComp } from "./pages/glassesComp";
 import { AboutUs } from "./pages/About";
-import { hookNavigate } from "./pages/hookNavigate";
 import { Cart } from "./pages/Cart";
+import { RoutesTest } from "./pages/RoutesTests";
 
 function App() {
   return (
@@ -15,33 +13,12 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/map" element={<Map />} />
         <Route path="/account" element={<Account />} />
-        <Route path="/glasses" element={<glassesComp />} />
-        <Route path="/About" element={<AboutUs />} />
-        <Route path="/hookNavigate" element={<hookNavigate />} />
-        <Route path="/Cart" element={<Cart />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/routes" element={<RoutesTest />} />
       </Routes>
     </BrowserRouter>
-
-
-
-  );
-  
-}
-
-function App() {
-  const catalogs = [
-    {
-      title: "Glasses",
-      items: ["Reading Glasses", "UV Protection", "Prescription Glasses"],
-      buttonId: "glasses-button",
-    },
-  ];
-
-  return (
-    <div>
-      {catalogs.map((catalog, index) => (
-        <Catalog key={index} {...catalog} />
-      ))}
-    </div>
   );
 }
+
+export default App;
