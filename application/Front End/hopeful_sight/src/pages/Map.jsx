eb5af0d03744923f61685f8871ad40ba2c01da38
student <ndavid@sfsu.edu>
2024-11-05 11:28:26 -0800

    Prototype Map

--- application/Front End/hopeful_sight/src/pages/Map.jsx
+++ application/Front End/hopeful_sight/src/pages/Map.jsx
@@ -1,3 +1,20 @@
-export function Map() {
-  return <p>Map Page</p>;
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
+import 'leaflet/dist/leaflet.css'
+const position = [51.505, -0.09]
+export function MapPage() {
+  return(
+    <div>
+      <MapContainer style={{height:"500px"}} center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  )
 }
